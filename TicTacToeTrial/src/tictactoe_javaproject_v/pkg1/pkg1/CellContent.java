9ca92a16203c3bf54cb4ff2049c11970044f3286
HagerElsayed <hagerelsayed39@gmail.com>
2017-02-27 15:10:01 +0200

    JavaFX Tic Tac Toe Playing in the same Device

---
+++ TicTacToeTrial/src/tictactoe_javaproject_v/pkg1/pkg1/CellContent.java
@@ -0,0 +1,15 @@
+package tictactoe_javaproject_v.pkg1.pkg1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hagerelsayed
+ */
+public enum CellContent {
+    X, O
+}
---
+++ TicTacToeTrial/src/tictactoe_javaproject_v/pkg1/pkg1/GameState.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe_javaproject_v.pkg1.pkg1;
+
+/**
+ *
+ * @author hagerelsayed
+ */
+public enum GameState {
+    PLAYING, DRAW, X_WON,O_WON
+}
